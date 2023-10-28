18a7556d8ace547dcb6889145b6e45b6c28f6c16
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-10-28 20:13:00 +0530

    Update truecaller_info.py

--- bot/plugins/handlers/truecaller_info.py
+++ bot/plugins/handlers/truecaller_info.py
@@ -1,6 +1,6 @@
 import re
 import traceback
-from pyrogram import Client, filters
+from pyrogram import Client, filters, types
 from pyrogram.types import Message
 from bot.config import Buttons
 from bot.utils import search_number
@@ -12,7 +12,7 @@ import html2text
 async def truecaller_info(client: Client, message: Message):
     ask = await message.chat.ask(
         text="Send me the number you want to search for.\n\n"
-        "Example: `+919876543210`\n"
+        "Example: +919876543210\n"
         "Only Indian numbers are supported.\n\n",
         filters=filters.text,
         timeout=3600,
@@ -29,24 +29,24 @@ async def truecaller_info(client: Client, message: Message):
     try:
         result = await search_number(ask.text)
     except Exception as e:
-        await message.reply_text(f"Error: `{e}`")
+        await message.reply_text(f"Error: {e}")
         return
 
     try:
-        data = result["data"][0]
+        data = result.data[0]
         text = f"""Information found on Truecaller for {ask.text}:
 
 Name: {data.get('name')}
 Gender: {data.get('gender')}
 Score: {data.get('score')}
-Carrier: {data.get('phones', [{}])[0].get('carrier') if data.get('phones') else None}
-Address: {data.get('addresses', [{}])[0].get('city') if data.get('addresses') else None} 
-Email: {data.get('internetAddresses', [{}])[0].get('id') if data.get('internetAddresses') else None}
+Carrier: {data.phones[0].carrier if data.phones else None}
+Address: {data.addresses[0].city if data.addresses else None}
+Email: {data.internetAddresses[0].id if data.internetAddresses else None}
 """
         await txt.edit(text=text, disable_web_page_preview=True)
     except Exception as e:
         traceback.print_exc()
-        await txt.edit(f"Error: `{e}`")
+        await txt.edit(f"Error: {e}")
 
 
 def html_to_markdown(html_content):
