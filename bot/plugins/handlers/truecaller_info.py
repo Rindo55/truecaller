4d7a6e0e91ed6ca62df65ecc819265d5e32dea65
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-10-29 15:58:34 +0530

    Update truecaller_info.py

--- bot/plugins/handlers/truecaller_info.py
+++ bot/plugins/handlers/truecaller_info.py
@@ -12,7 +12,7 @@ import json
 async def truecaller_info(client: Client, message: Message):
     ask = await message.chat.ask(
         text="Send me the number you want to search for.\n\n"
-        "Example: +919876543210\n"
+        "Example (Format): +919876543210\n"
         "Only Indian numbers are supported.\n\n",
         filters=filters.text,
         timeout=3600,
@@ -21,25 +21,26 @@ async def truecaller_info(client: Client, message: Message):
     regex = r"^\+?[1-9]\d{1,14}$"
 
     if not re.search(regex, ask.text):
-        await message.reply_text("Invalid number! Please try again.")
+        await message.reply_text("Invalid number! Please try again and see the example.")
         return
 
     txt = await message.reply_text("Searching for the number...")
 
     try:
         result = await search_number(ask.text)
-        print(result)
     except Exception as e:
         await message.reply_text(f"Error : `{e}`")
         return
     try:
-        data = result["data"]
+        Score = float(result["data"]["data"][0]["score"])
+        score_Percent = Score*100
         text = f"""Information found on Truecaller for {ask.text}:
+        
 Name: {result["data"]["data"][0]["name"]}
-Score: {result["data"]["data"][0]["score"]}
-Carrier: {result["data"]["data"][0]["phones"][0]["carrier"] if result["data"]["data"][0]["phones"][0] else None}
-Address: {result["data"]["data"][0]["addresses"][0]["city"] if result["data"]["data"][0]["addresses"][0] else None} 
-Email: {result["data"]["data"][0]["internetAddresses"][0]["id"] if result["data"]["data"][0]["internetAddresses"][0] else None}
+Score: {score_Percent}%
+Carrier: {result["data"]["data"][0]["phones"][0]["carrier"] if result["data"]["data"][0]["phones"] else None}
+Address: {result["data"]["data"][0]["addresses"][0]["city"] if result["data"]["data"][0]["addresses"] else None} 
+Email: {result["data"]["data"][0]["internetAddresses"][0]["id"] if result["data"]["data"][0]["internetAddresses"] else None}
 """
         await txt.edit(text=text, disable_web_page_preview=True)
     except Exception as e:
